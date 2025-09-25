db4c129bed59692082bd17c17626b37f8765b085
pavan <pavan06160@gmail.com>
2025-09-25 12:33:12 -0500

    variables

---
+++ 04-read-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ===================================================
+# Script 4: Read-only Variables
+# ===================================================
+readonly school="UWM"
+echo "School is $school"
+
+# school="Another"   # ❌ Error: cannot modify readonly variable
+
