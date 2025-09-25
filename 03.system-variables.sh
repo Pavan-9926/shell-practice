d340cdd9e1ffa2371764454843ad11e7c907852f
pavan <pavan06160@gmail.com>
2025-09-25 12:20:36 -0500

    variables

---
+++ 03.system-variables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# ===================================================
+# Script 3: System Variables
+# ===================================================
+# Bash provides many built-in variables.
+
+echo "Current shell: $SHELL"
+echo "Home directory: $HOME"
+echo "Current working directory: $PWD"
+echo "User: $USER"
+echo "Script name: $0"
+echo "Number of arguments: $#"
+echo "All arguments: $@"
+echo "Process ID of current script: $$"
+
+
+echo "Current shell: $SHELL"
+echo "Home directory : $HOME"
+echo "Current working directory : $PWD"
+echo "User : $USER"
+echno "Script name : $0"
+echno "Number of arguments: $#"
+echo "All Arguments : $@"
+echo "Process ID of the current script : $$"
