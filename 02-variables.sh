ba26711322d8552f47fed9e0b0532f8d14824024
pavan <pavan06160@gmail.com>
2025-09-25 11:43:22 -0500

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ===================================================
+# Script 2: Variable Naming Rules
+# ===================================================
+# Rules:
+#  - Must start with a letter or underscore
+#  - Cannot start with a number
+#  - No spaces allowed
+#  - Case-sensitive
+
+myVar="hello"
+MyVar="world"
+
+echo "myVar = $myVar"
+echo "MyVar = $MyVar"
+
